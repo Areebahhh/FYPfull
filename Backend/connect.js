572e78b6c2f409f43ee318afacc27053fdce66b4
Areebah Binte Suhail <areebahsohail02@gmail.com>
2024-05-25 16:33:22 +0500

    merged code

--- Backend/connect.js
+++ Backend/connect.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"",
-  database:"fyp"
+  password:"Areebahh7777",
+  database:"fypfinal"
 })
